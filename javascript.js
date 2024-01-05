fc6eb2dce2dcfe14652a3fadc09c285f75b74aa5
WorriorBeast <128549074+WorriorBeast@users.noreply.github.com>
2024-01-04 20:55:14 -0800

    Capitalize first letter of each word in each text input

--- javascript.js
+++ javascript.js
@@ -20,6 +20,8 @@ form.addEventListener('submit', function(e) {
     const book = new Book(textInput[0].value, textInput[1].value, textInput[2].value, textInput[3].value,
                 pages.value, read);
 
+    book.capitalizeWord();
+    
     library.push(book);
 
     this.reset();
@@ -33,3 +35,32 @@ function Book(title, author, genre, language, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+
+Book.prototype.capitalizeWord = function() {
+    let bookTitle = this.title;
+    let splitTitle = bookTitle.split(' ');
+
+    let bookAuthor = this.author;
+    let splitAuthor = bookAuthor.split( ' ');
+
+    let bookGenre = this.genre;
+    let splitGenre = bookGenre.split(' ');
+
+    let bookLanguage = this.language;
+    let splitLanguage = bookLanguage.split(' ');
+
+    for (i = 0; i < splitTitle.length; i++) {
+        splitTitle[i] = splitTitle[i].at(0).toUpperCase() + splitTitle[i].slice(1);
+
+        splitAuthor[i] = splitAuthor[i].at(0).toUpperCase() + splitAuthor[i].slice(1);
+        
+        splitGenre[i] = splitGenre[i].at(0).toUpperCase() + splitGenre[i].slice(1);
+        
+        splitLanguage[i] = splitLanguage[i].at(0).toUpperCase() + splitLanguage[i].slice(1);
+    }
+
+    this.title = splitTitle.join(' ');
+    this.author = splitAuthor.join(' ');
+    this.genre = splitGenre.join(' ');
+    this.language = splitLanguage.join(' ');
+};
